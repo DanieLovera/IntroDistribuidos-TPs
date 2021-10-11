e21abef9ea7949caf3e2807d2ec72636b349e978
Daniel Lovera <daanilo2497@gmail.com>
2021-10-11 02:50:48 -0300

    Devuelvo el ultimo cambio mal cargado

--- FTP_TP1/client/client.py
+++ FTP_TP1/client/client.py
@@ -5,26 +5,7 @@ script_dir = os.path.dirname(__file__)
 mymodule_dir = os.path.join(script_dir, '..', 'common')
 sys.path.append(mymodule_dir)
 from socket_tcp import SocketTCP
-from socket_interface import ISocket
-from comm_protocol import CommProtocol
-
-def do_send(comm_protocol: CommProtocol):
-    msg = "pablito clavo un clavito que clavito clavo pablito"
-    comm_protocol.send(msg.encode())
-
-def do_recv(comm_protocol: CommProtocol):
-    return comm_protocol.recv().decode()
 
 if __name__ == "__main__":
     print("main real... deberian ser dos")
     print("upload file y download file")
-
-    HOST = "localhost"
-    PORT = 7777
-
-    with SocketTCP() as peer:
-        comm_protocol = CommProtocol(peer)
-        peer.connect(HOST, PORT)
-        do_send(comm_protocol)
-        data = do_recv(comm_protocol)
-    print('Recibi el mensaje:', data)
--- FTP_TP1/server/server.py
+++ FTP_TP1/server/server.py
@@ -5,30 +5,7 @@ script_dir = os.path.dirname(__file__)
 mymodule_dir = os.path.join(script_dir, '..', 'common')
 sys.path.append(mymodule_dir)
 from socket_tcp import SocketTCP
-from socket_interface import ISocket
-from comm_protocol import CommProtocol
-import socket
-
-def do_send(comm_protocol: CommProtocol):
-    msg = "Npi"
-    comm_protocol.send(msg.encode())
-
-def do_recv(comm_protocol: CommProtocol):
-    return comm_protocol.recv().decode()
 
 if __name__ == "__main__":
     print("main real... deberian ser server-start")
 
-    HOST = "localhost"
-    PORT = 7777
-    MAX_CONNECTIONS = 10
-
-    with SocketTCP() as listener:
-        listener.bind(HOST, PORT)
-        listener.listen(MAX_CONNECTIONS)
-        peer = listener.accept()
-        with peer:
-            comm_protocol = CommProtocol(peer)
-            data = do_recv(comm_protocol)
-            do_send(comm_protocol)
-            print('Recibi el mensaje:', data)
