01bbbbff489641aa9d12fcf174fe401dc48f0142
Daniel Lovera <daanilo2497@gmail.com>
2021-10-10 21:19:35 -0300

    Modifico archivitos de pruebas

--- FTP_TP1/tests/serverTCP_test.py
+++ FTP_TP1/tests/serverTCP_test.py
@@ -5,8 +5,18 @@ script_dir = os.path.dirname(__file__)
 mymodule_dir = os.path.join(script_dir, '..', 'common')
 sys.path.append(mymodule_dir)
 from socket_tcp import SocketTCP
+from socket_interface import ISocket
+from comm_protocol import CommProtocol
+
+def do_send(comm_protocol: CommProtocol):
+    msg = "Npi"
+    comm_protocol.send(msg.encode())
+
+def do_recv(comm_protocol: CommProtocol):
+    return comm_protocol.recv().decode()
 
 if __name__ == "__main__":
+
     HOST = "localhost"
     PORT = 7777
     MAX_CONNECTIONS = 10
@@ -16,10 +26,7 @@ if __name__ == "__main__":
         listener.listen(MAX_CONNECTIONS)
         peer = listener.accept()
         with peer:
-            data = peer.recv(4)
-            peer.send(b"Hola")
-            print('Received', data)
-            """while data:
-                print('Received', data)
-                peer.send(b"Hola")
-                data = peer.recv(4)"""
+            comm_protocol = CommProtocol(peer)
+            data = do_recv(comm_protocol)
+            do_send(comm_protocol)
+            print('Recibi el mensaje:', data)
