a4b6d0103589760dbad9f0e2f6be85668749fa35
Alex <garbieto@fi.uba.ar>
2021-10-29 04:24:27 -0500

    calculo del timeout corregido

--- FTP_TP1/common/gbntp.py
+++ FTP_TP1/common/gbntp.py
@@ -7,7 +7,7 @@ class GBNTP:
     SEQ_NUM_SIZE = 1
     WINDOW_SIZE = 4
     MAX_SEQ_NUM = 2 * WINDOW_SIZE
-    MAX_DATAGRAM_SIZE = 64000    # 64kb
+    MAX_DATAGRAM_SIZE = 20    # 64kb
 
     def __init__(self, socket):
         self.sender_base = 1
@@ -37,6 +37,13 @@ class GBNTP:
 
     def update_state(self, seq_num_received):
         # Clean all the packets acknowledged and their timers
+        self.timeout.calculateTimeout(
+            now() - 
+            self.time_started[
+                (seq_num_received - self.sender_base)%self.MAX_SEQ_NUM
+            ]
+        )
+
         for _ in range((seq_num_received - self.sender_base + 1)%self.MAX_SEQ_NUM):
             self.not_acknowledged.pop(0)
             self.time_started.pop(0)
@@ -52,12 +59,12 @@ class GBNTP:
 
     def _send_a_packet(self, data: bytearray, host, port, last_send: bool):
         sent = 0
-        # print("Envio un pedazo:")
-        # print(data)
+        print("Envio un pedazo:")
+        print(data)
 
         if self.send_in_window():
-            # print("numero de sequencia enviando")
-            # print(self.sender_seq_num)
+            print("numero de sequencia enviando")
+            print(self.sender_seq_num)
             self.not_acknowledged.append(
                 self.__pack(self.sender_seq_num, data)
             )
@@ -77,13 +84,12 @@ class GBNTP:
                     self.socket.settimeout(timeout)
                     pkt_received, _ = self.socket.recvfrom(self.SEQ_NUM_SIZE)
                     seq_num_received, _ = self.__unpack(pkt_received)
-                    # print("numero de secuencia recivido")
-                    # print(seq_num_received)
+                    print("numero de secuencia recivido")
+                    print(seq_num_received)
                     if seq_num_received < self.sender_base:
                         continue
 
                     self.update_state(seq_num_received)
-                    self.timeout.calculateTimeout(now() - self.time_started[0])
                     advanced = True
 
                 except socket.timeout:
@@ -93,15 +99,15 @@ class GBNTP:
                         self.time_started.append(now())
                         self.socket.sendto(p, (host, port))
 
-            sent = self.send(data, host, port, last_send)
+            sent = self._send_a_packet(data, host, port, last_send)
 
         # Ensure all the data had been sent to destination
         # because there won't be more send
         if last_send:
-            # print("-----------Ultimo envio-----------")
+            print("-----------Ultimo envio-----------")
             while not len(self.not_acknowledged) == 0:
-                # print("tamaño")
-                # print(len(self.not_acknowledged))
+                print("tamaño")
+                print(len(self.not_acknowledged))
                 try:
                     timeout = self.timeout.getTimeout() - (now() - self.time_started[0])
                     if timeout <= 0:
@@ -110,28 +116,27 @@ class GBNTP:
                     self.socket.settimeout(timeout)
                     pkt_received, _ = self.socket.recvfrom(self.SEQ_NUM_SIZE)
                     seq_num_received, _ = self.__unpack(pkt_received)
-                    # print("numero de secuencia recivido")
-                    # print(seq_num_received)
+                    print("numero de secuencia recivido")
+                    print(seq_num_received)
                     if seq_num_received < self.sender_base:
                         continue
 
                     self.update_state(seq_num_received)
-                    self.timeout.calculateTimeout(now() - self.time_started[0])
                 except socket.timeout:
                     self.timeout.timeout()
-                    # print("Reenviando paquetes")
+                    print("Reenviando paquetes")
                     self.time_started.clear()
                     for p in self.not_acknowledged:
-                        # print("Reenviando paquet")
-                        # print(p)
+                        print("Reenviando paquet")
+                        print(p)
                         self.time_started.append(now())
                         self.socket.sendto(p, (host, port))
 
         return sent
 
     def send(self, data: bytes, host, port):
-        # print("Quiero enviar")
-        # print(data)
+        print("Quiero enviar")
+        print(data)
         _data = bytearray(data)
         sent = 0
 
@@ -139,7 +144,7 @@ class GBNTP:
             last_send = False
 
             if i + self.MAX_DATAGRAM_SIZE > len(_data):
-                # print("Ultimo pedazo")
+                print("Ultimo pedazo")
                 last_send = True
 
             sent += self._send_a_packet(
@@ -153,17 +158,17 @@ class GBNTP:
         return sent
 
     def _recv(self, buffsize):
-        # print("quiero recibir en pedazo estos bytes:")
-        # print(buffsize)
+        print("quiero recibir en pedazo estos bytes:")
+        print(buffsize)
         correct_seq_numb = False
         while not correct_seq_numb:
             pkt_received, source = self.socket.recvfrom(buffsize + self.SEQ_NUM_SIZE)
             seq_num_received, data_received = self.__unpack(pkt_received)
 
-            # print("numero de sequencia recibido")
-            # print(seq_num_received)
-            # print("numero de sequencia esperado")
-            # print(self.receiver_seqnum)
+            print("numero de sequencia recibido")
+            print(seq_num_received)
+            print("numero de sequencia esperado")
+            print(self.receiver_seqnum)
 
             if seq_num_received == self.receiver_seqnum and len(data_received) == buffsize:
                 pkt = self.__pack(self.receiver_seqnum, b'')
@@ -171,28 +176,28 @@ class GBNTP:
                 self.receiver_seqnum = self.next(self.receiver_seqnum)
                 correct_seq_numb = True
             else:
-                # print("Reenviando el ack")
-                # print(self.prev(self.receiver_seqnum))
+                print("Reenviando el ack")
+                print(self.prev(self.receiver_seqnum))
                 pkt = self.__pack(self.prev(self.receiver_seqnum), b'')
                 self.socket.sendto(pkt, source)
         
         return data_received, source
 
     def recv(self, buffsize):
-        # print("quiero recibir estos bytes:")
-        # print(buffsize)
+        print("quiero recibir estos bytes:")
+        print(buffsize)
         received = 0
         data = []
         for i in range(0, buffsize, self.MAX_DATAGRAM_SIZE):
-            # print("-------Iteracion numero-------")
-            # print(i)
+            print("-------Iteracion numero-------")
+            print(i)
             d, s = self._recv(min(self.MAX_DATAGRAM_SIZE, buffsize - i))
-            # print("data")
-            # print(d)
+            print("data")
+            print(d)
             data.append(d)
             received += len(d)
 
         data = b''.join(data)
-        # print("received")
-        # print(data)
+        print("received")
+        print(data)
         return data, s
